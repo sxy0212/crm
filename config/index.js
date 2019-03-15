057f9de6e9de32c8ea9104f878da3a800d80087d
sxy <1043382831@qq.com>
2019-03-15 10:25:54 +0800

    .

--- config/index.js
+++ config/index.js
@@ -46,10 +46,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../test/agent/index.html'),
+    index: path.resolve(__dirname, '../agent/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../test/agent'),
+    assetsRoot: path.resolve(__dirname, '../agent'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
